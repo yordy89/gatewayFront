48ed8581812ec07f7cc1afc84b907f53df52d942
Yordanys Albelo <yordanys.albelo@levatas.com>
2021-05-27 08:56:06 -0400

    Fix store issues

--- src/Hooks/useSetGateway.js
+++ src/Hooks/useSetGateway.js
@@ -1,4 +1,4 @@
-import { useState, useEffect, useCallback } from 'react'
+import { useState, useEffect } from 'react'
 import { useDispatch } from 'react-redux'
 import { addGateway, editGateway } from 'Store/reducer'
 
--- src/Store/store.js
+++ src/Store/store.js
@@ -1,4 +1,4 @@
-import { combineReducers, createStore, applyMiddleware, compose} from 'redux'
+import { combineReducers, createStore, applyMiddleware, compose } from 'redux'
 import thunk from 'redux-thunk'
 import GatewayReducer from './reducer'
 
@@ -8,12 +8,12 @@ const reducers = combineReducers({
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 
-export default function configureStore(){
+export default function configureStore() {
     return createStore(
         reducers,
         compose(
             applyMiddleware(thunk),
-            window.__REDUX_DEVTOOLS_EXTENSION__ ? window.__REDUX_DEVTOOLS_EXTENSION__() : ''
-            )
+            composeEnhancers
+        )
     )
 }
